dadbed9c4e80400e1d54b267a4aa09960fe6724c
kaisda <ftcyang@gmail.com>
2024-07-10 13:55:43 +0800

    大功告成

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from myapp.models import ProductModel, OrderModel, DetailModel 
+from myapp.models import ProductModel, OrderModel, DetailModel
+from smtplib import SMTP
+from email.mime.text import MIMEText
 
 # Create your views here.
 
@@ -49,28 +51,27 @@ def addtocart(request, type=None, id=None): # type指定購物車的四種行為
         # 用 True False 來判斷有沒有存在商品
         noCartSession = True
         for unit in cartlist:
-                if product.pname == unit[0]:
-                     unit[2] = str(int(unit[2]) + 1)
-                     unit[3] = str(int(unit[3])+product.pprice*1)
-                     noCartSession = False
+            if product.pname == unit[0]:
+                unit[2] = str(int(unit[2]) + 1)
+                unit[3] = str(int(unit[3])+product.pprice*1)
+                noCartSession = False
         if noCartSession:
             templist = []
-            
             # (pname, pprice, 1, 1*pprice)
             templist.append(product.pname)
             templist.append(str(product.pprice))
             templist.append(str(1))
             templist.append(str(product.pprice*1))
-            
+            # 全域變數
             cartlist.append(templist)
         request.session['cartlist'] = cartlist
         # print(request.session['cartlist']) # 把session 資料列印到終端機顯示
         # return HttpResponse('使用者已選購商品') # 在頁面顯示加入成功資訊
         return redirect('/cart/')
     elif type == 'empty':
-         cartlist = []                          # 清空購物車資訊
-         request.session['cartlist']=cartlist   # 重設session
-         return redirect('/cart/')              # 重新導回購物車頁面
+        cartlist = []                          # 清空購物車資訊
+        request.session['cartlist']=cartlist   # 重設session
+        return redirect('/cart/')              # 重新導回購物車頁面
     elif type == 'update':
         if request.method == 'POST':
             n = 1
@@ -93,13 +94,12 @@ def cart(request):
     global shipping             # 運費 shipping 全域變數
     localcartlist = cartlist    # 將全域變數改為區域變數, 會被locals() 傳送到網頁
     localshipping = shipping    # 將全域變數改為區域變數, 會被locals() 傳送到網頁
-    
     total = 0                           # 金額小計, 會被locals()傳送到網頁
+    
     for unit in cartlist:
          total = total + int(unit[3])
 
     grandtotal = total + localshipping  # 總價(金額+運費), 會被locals()傳送到網頁
-
     # print(cartlist) # 把 cartlist 的資料列印到終端機顯示
     return render(request, 'cart.html', locals())
 
@@ -127,7 +127,7 @@ def cartok(request):
         total = 0
         for unit in cartlist:
             total = total + int(unit[3])
-            grandtotal = total + shipping
+        grandtotal = total + shipping
 
     #-- 將訂購人資訊寫入OrderModel表單 ---
     productOrder = OrderModel.objects.create(customername = customername, customerphone = customerphone, customeremail = customeremail, customeraddress = customeraddress, paytype = paytype, grandtotal = grandtotal, shipping = shipping, subtotal = total)
@@ -144,5 +144,26 @@ def cartok(request):
     #-- 訂單送出後清空購物車 ---
     cartlist = []
 
+
+    #-- 郵件寄送
+    mailfrom = 'ftcyang@gmail.com'
+    mailpw = 'mkwmbiigidlsmkju'
+    mailto = customeremail
+    mailsubject = '木葉商城訂單通知'
+    mailcontent = customername + ', 您所選取的商品已訂購成功, 我們會盡速且祕密的送至您指定的地點, 感謝您的支持, 歡迎隨時回來 !'
+    send_message(mailfrom, mailpw, mailto, mailsubject, mailcontent)
+
     return render(request, 'cartok.html', locals())
     
+def send_message(mailfrom, mailpw, mailto, mailsubject, mailcontent):
+    strSmtp = 'smtp.gmail.com:587'
+    strAccount = mailfrom
+    strPassword = mailpw
+    msg = MIMEText(mailcontent)
+    msg['Subject'] = mailsubject
+    mailto1 = mailto
+    server = SMTP(strSmtp)
+    server.ehlo()       # 與主機溝通
+    server.starttls()   # TTLS安全認證
+    server.login(strAccount, strPassword)
+    server.sendmail(strAccount, mailto1, msg.as_string())
--- narutomarket/urls.py
+++ narutomarket/urls.py
@@ -21,6 +21,7 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('', views.index),
     path('detail/<int:id>/', views.detail),
     # path('set_cookie/<str:key>/<str:value>/', views.set_cookie),
     # path('set_session/<str:key>/<str:value>/', views.set_session),
